119daf07dadc0a12b86d64b2507c6bdee6394670
Jurgen Hasmeta <jurgenhasmeta12@yahoo.com>
2021-11-18 15:12:16 +0100

    Part 1 finished

--- index.js
+++ index.js
@@ -44,3 +44,12 @@ for (let element of users) {
     console.log(`Hi, this user id is : ${element.id} \n and the user name is : ${element.name} \n and the user city is : ${element.address.city} `)
     }
 }
+alert(`Now we will display the user with name : ${users[userInputId - 1].name} \n and his id : ${userInputId} \n his all TODOLIST and all infos about it `)
+
+//Here we check for todos of userIputId from the prompt and we get all todos from that user along with the name of the user also status
+for (let element of todos) {
+    if (element.userId === userInputId) {
+        alert(`The todolist of name : ${users[userInputId - 1].name} \n and the id of todo is : ${element.id} \n and the titles are : ${element.title} \n and the status of it is : ${element.completed}`)
+        console.log(`The todolist of name : ${users[userInputId - 1].name} \n and the id of todo is : ${element.id} \n and the titles are : ${element.title} \n and the status of it is : ${element.completed}`)
+    }
+}
