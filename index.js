e8e755477c360e18b4efe1bca6e0a0dbbbd3709b
Jurgen Hasmeta <jurgenhasmeta12@yahoo.com>
2021-11-18 15:59:36 +0100

    working on challenge

--- index.js
+++ index.js
@@ -44,12 +44,53 @@ for (let element of users) {
     console.log(`Hi, this user id is : ${element.id} \n and the user name is : ${element.name} \n and the user city is : ${element.address.city} `)
     }
 }
-alert(`Now we will display the user with name : ${users[userInputId - 1].name} \n and his id : ${userInputId} \n his all TODOLIST and all infos about it `)
 
-//Here we check for todos of userIputId from the prompt and we get all todos from that user along with the name of the user also status
 for (let element of todos) {
-    if (element.userId === userInputId) {
-        alert(`The todolist of name : ${users[userInputId - 1].name} \n and the id of todo is : ${element.id} \n and the titles are : ${element.title} \n and the status of it is : ${element.completed}`)
-        console.log(`The todolist of name : ${users[userInputId - 1].name} \n and the id of todo is : ${element.id} \n and the titles are : ${element.title} \n and the status of it is : ${element.completed}`)
+    var count = 0
+    if (element.id === userInputId) {
+        count++
+    }
+}
+
+
+//Here we ask what want use to do with this id, add a todolist or just show 
+let userChoiceMenu = prompt(`Would you like to show the todoslist of this user ${users[userInputId - 1].name} \n or want to update the todolist with adding one there \n so either "ADD" or "SHOW" ???`)
+
+if (userChoiceMenu === "ADD" || userChoiceMenu === "add") {
+    let userTitleToDo = prompt("Enter title of TODO wich you want to add : ")
+    let userCompletedToDo = prompt("Enter if you have finished it of TODO wich you want to add so its '1' or '0' : ")
+    
+    const userInputObject = {
+        userId : userInputId,
+        id: todos[userInputId - 1].id,
+        title: userTitleToDo,
+        completed: userCompletedToDo
+    }
+
+    alert(`Now we will update the todolist of the user with name : ${users[userInputId - 1].name} \n and his id : ${userInputId} \n and ADD an todolist item to this user`)
+    alert(`Now user is updated with a new todolist wich is : ${userTitleToDo} \n and ${Boolean(userCompletedToDo)}`)
+    console.log(`Now user is updated with a new todolist wich is : ${userTitleToDo} \n and ${Boolean(userCompletedToDo)}`)
+    alert("Now we will show them all")
+    
+    for (let element of todos) {
+        if (element.userId === userInputId && element.id >= count) {
+            todos.push(userInputObject)
+            alert(`The todolist of name : ${users[userInputId - 1].name} \n and the id of todo is : ${element.id} \n and the titles are : ${element.title} \n and the status of it is : ${element.completed}`)
+            console.log(`The todolist of name : ${users[userInputId - 1].name} \n and the id of todo is : ${element.id} \n and the titles are : ${element.title} \n and the status of it is : ${element.completed}`)
+        }
+    }
+}
+
+
+//If user just want to show and display data of that user with that id his todolist
+else if (userChoiceMenu === "SHOW" || userChoiceMenu === "show") {
+    alert(`Now we will display the user with name : ${users[userInputId - 1].name} \n and his id : ${userInputId} \n his all TODOLIST and all infos about it `)
+    
+    //Here we check for todos of userIputId from the prompt and we get all todos from that user along with the name of the user also status
+    for (let element of todos) {
+        if (element.userId === userInputId) {
+            alert(`The todolist of name : ${users[userInputId - 1].name} \n and the id of todo is : ${element.id} \n and the titles are : ${element.title} \n and the status of it is : ${element.completed}`)
+            console.log(`The todolist of name : ${users[userInputId - 1].name} \n and the id of todo is : ${element.id} \n and the titles are : ${element.title} \n and the status of it is : ${element.completed}`)
+        }
     }
 }
