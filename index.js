6e34de09c435b2c8d6d4f19fe007f70cf6acc4a6
Jurgen Hasmeta <jurgenhasmeta12@yahoo.com>
2021-11-18 17:23:26 +0100

    challenge 2

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ for (let element of users) {
 // }
 
 //Here we ask what want use to do with this id, add a todolist or just show 
-let userChoiceMenu = prompt(`Would you like to show the todoslist of this user ${users[userInputId - 1].name} \n or want to update the todolist with adding one there \n so either "ADD" or "SHOW" ???`)
+let userChoiceMenu = prompt(`Would you like to show the todoslist of this user ${users[userInputId - 1].name} \n or want to update the todolist with adding one there \n or want to update the todolist with updating one there \n or want to delete something from the todolist with adding one there \n Or do you want to change the user \n Or do you want to terminate the program \n so either "ADD" or "SHOW" or "DELETE" or "UPDATE" or "CLOSE" or "CHANGE USER" ???`)
 
 if (userChoiceMenu === "ADD" || userChoiceMenu === "add") {
 
@@ -101,4 +101,59 @@ else if (userChoiceMenu === "SHOW" || userChoiceMenu === "show") {
             console.log(`The todolist of name : ${users[userInputId - 1].name} \n and the id of todo is : ${element.id} \n and the titles are : ${element.title} \n and the status of it is : ${element.completed}`)
         }
     }
+}
+
+else if (userChoiceMenu === "DELETE" || userChoiceMenu === "delete") {
+    let userChoiceDeleteToDoUser = Number(prompt("Enter the id of the user wich you want to delete: from 1-10 "))
+    let userChoiceDeleteToDoId = Number(prompt("Enter the id of the todo wich you want to delete from this user: "))
+    alert("We will now delete this todo from database")
+
+    for (let element of todos) {
+        if (element.userId === userChoiceDeleteToDoUser && element.id === userChoiceDeleteToDoId) {
+            delete element.title
+            delete element.userId
+            delete element.id
+            delete element.completed
+        }
+    }
+
+    for (let element of todos) {
+        if (element.userId === userChoiceDeleteToDoUser) {
+            alert(`The todolist of name : ${users[userChoiceDeleteToDoUser - 1].name} \n and the id of todo is : ${element.id} \n and the titles are : ${element.title} \n and the status of it is : ${element.completed}`)
+            console.log(`The todolist of name : ${users[userChoiceDeleteToDoUser - 1].name} \n and the id of todo is : ${element.id} \n and the titles are : ${element.title} \n and the status of it is : ${element.completed}`)
+        }
+    }
+
+}
+
+
+else if (userChoiceMenu === "UPDATE" || userChoiceMenu === "update") {
+    let userChoiceUpdateToDoUser = Number(prompt("Enter the id of the user wich you want to update: from 1-10 "))
+    let userChoiceUpdateToDoId = Number(prompt("Enter the id of the todo wich you want to update from this user: "))
+    let userChoiceUpdateToDoTitle = prompt("Enter the title of this todo ")
+    let userChoiceUpdateToDoCompleted = Boolean(prompt("Enter status of the  update todo '1' or '0': "))
+
+    alert("We will now update this todo from database")
+
+    for (let element of todos) {
+        if (element.userId === userChoiceUpdateToDoUser && element.id === userChoiceUpdateToDoId) {
+            element.title = userChoiceUpdateToDoTitle
+            element.completed = userChoiceUpdateToDoCompleted
+        }
+    }
+
+    for (let element of todos) {
+        if (element.userId === userChoiceUpdateToDoUser) {
+            alert(`The todolist of name : ${users[userChoiceUpdateToDoUser - 1].name} \n and the id of todo is : ${element.id} \n and the titles are : ${element.title} \n and the status of it is : ${element.completed}`)
+            console.log(`The todolist of name : ${users[userChoiceUpdateToDoUser - 1].name} \n and the id of todo is : ${element.id} \n and the titles are : ${element.title} \n and the status of it is : ${element.completed}`)
+        }
+    }
+}
+
+else if (userChoiceMenu === "CHANGE USER" || userChoiceMenu === "change user") {
+    userInputId = Number(prompt("Please enter an id wich you want to look into from 1-10 in users database : "))
+}
+
+else if (userChoiceMenu === "CLOSE" || userChoiceMenu === "close") {
+    throw new Error("User wanted to close the program !")
 }
